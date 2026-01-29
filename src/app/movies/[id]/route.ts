e90c9e047ed70dacd36dc3738ef2953379b4ee47
betsaleeldjuma <djumabestaleel@gmail.com>
2026-01-29 18:15:33 +0200

    Update

--- src/app/movies/[id]/route.ts
+++ src/app/movies/[id]/route.ts
@@ -7,3 +7,14 @@ export async function GET(_req: Request, {params}: {params: {id: string}}) {
 
     return movie ? new Response(JSON.stringify(movie)) : new Response('Movie not found', {status: 404})
 }
+
+export async function PATCH(req: Request, {params}: {params: {id: string}}) {
+    const {id} = await params;
+    const movie = movies.find(movie => movie.id === +id);
+
+    if(!movie) return new Response('Movie not found', {status: 404});
+
+    const body = await req.json();
+
+    const updatedTitle = body.title ? {movie.name = body.title} : movie.name;
+}
