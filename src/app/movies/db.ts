8652822b68735ff7d98e8c344bc00d2c3c23369f
betsaleeldjuma <djumabestaleel@gmail.com>
2026-01-29 18:05:06 +0200

    Update

---
+++ src/app/movies/db.ts
@@ -0,0 +1,28 @@
+export const movies = [
+    {
+        id: 1,
+        name: 'John Wick',
+        ratings: 1,
+        genre: 'Action'
+    }, {
+        id: 2,
+        name: 'The Shawshank Redemption',
+        ratings: 2,
+        genre: 'Drama'
+    }, {
+        id: 3,
+        name: 'The Godfather',
+        ratings: 3,
+        genre: 'Drama'
+    }, {
+        id: 4,
+        name: 'The Dark Knight',
+        ratings: 4,
+        genre: 'Action'
+    }, {
+        id: 5,
+        name: "Increption",
+        ratings: 5,
+        genre: 'Action'
+    }
+]
