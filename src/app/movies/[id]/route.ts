5125e872b7db22d85077b9cf94bfa69436e3db37
betsaleeldjuma <djumabestaleel@gmail.com>
2026-01-31 07:52:14 +0200

    Update

--- src/app/movies/[id]/route.ts
+++ src/app/movies/[id]/route.ts
@@ -7,3 +7,31 @@ export async function GET(_req: Request,{params} : {params: {id: string}}) {
 
     return movie ? new Response(JSON.stringify(movie)) : new Response("Movie not found", {status: 404})
 }
+
+export async function PATCH(req: Request, {params}: {params: {id: string}}) {
+    const {id} = params;
+    const movieId = +id;
+
+    const movie = movies.find((m) => m.id === movieId);
+
+    if(!movie) {
+        return new Response(JSON.stringify({error: 'Movie Not Found :('}), {status: 404})
+    }
+
+    try {
+        const updatedMovie = await req.json();
+
+        const index = movies.findIndex(m => m.id === movieId)
+
+        if(!movie) {
+            return new Response(JSON.stringify({error: 'Movie Not Found :('}), {status: 404})
+        }
+
+        // Update the movei
+        movies[index] = {...movie, ...updatedMovie}
+
+        return new Response(JSON.stringify(movies[index]), {status: 200})
+    } catch (error) {
+        return new Response(JSON.stringify({error: 'Failed to parse JSON'}), {status: 404})
+    }
+}
