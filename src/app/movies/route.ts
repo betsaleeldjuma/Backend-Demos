f99d5f85e59c42d1c89e834f68ffaae9efad0f1e
betsaleeldjuma <djumabestaleel@gmail.com>
2026-02-01 17:36:56 +0200

    Update

--- src/app/movies/route.ts
+++ src/app/movies/route.ts
@@ -2,11 +2,12 @@
 //     return new Response('Hello Wold')
 // }
 
+import { NextRequest } from "next/server";
 import { movies } from "./db";
 
-export async function GET() {
-    return Response.json(movies)
-}
+// export async function GET() {
+//     return Response.json(movies)
+// }
 
 export async function POST(req: Request) {
     let movie = await req.json();
@@ -15,4 +16,13 @@ export async function POST(req: Request) {
     movies.push(newMovie)
 
     return new Response(JSON.stringify(newMovie))
+}
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get('query')
+
+    const filteredMovie = query ? movies.filter(m => m.name.toLowerCase().includes(query)) : movies;
+
+    return new Response(JSON.stringify(filteredMovie))
 }
